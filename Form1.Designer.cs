50b11ffa4e5e1766c82d14d42140358a8bd104c4
Yash Narang <yashnarang1996@gmail.com>
2021-02-20 18:37:04 +0530

    Image Editor first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Image_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1132, 537);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1135, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1133, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1133, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Filter 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1133, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Filter 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1133, 299);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 41);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Filter 3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1133, 158);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 41);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Reset Image";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1133, 346);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 41);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Rotate Image";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1133, 393);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 41);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Flip Image X Axis";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(1133, 440);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 41);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Flip Image Y Axis";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 666);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Image_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Image file;
+        Boolean opened = false;
+
+        void openImage()
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+        }
+
+        void saveImage()
+        {
+            if (opened)
+            {
+                SaveFileDialog sfd = new SaveFileDialog(); 
+                sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+                ImageFormat format = ImageFormat.Png;
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string ext = Path.GetExtension(sfd.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    pictureBox1.Image.Save(sfd.FileName, format);
+                }
+
+
+
+            }
+            else { MessageBox.Show("No image loaded, first upload image "); }
+
+        }
+
+        void reload()
+        {
+            if (!opened)
+            {
+                // MessageBox.Show("Open an Image then apply changes");
+            }
+            else
+            {
+                if (opened)
+                {
+                    file = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.Image = file;
+                    opened = true;
+                }
+            }
+        }
+
+        void filter1()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("Open an Image then apply changes");
+            }
+            else
+            {
+
+                Image img = pictureBox1.Image;                             
+                Bitmap bmpInverted = new Bitmap(img.Width, img.Height);  
+                                                                        
+
+                ImageAttributes ia = new ImageAttributes();                 
+                ColorMatrix cmPicture = new ColorMatrix(new float[][]       
+                {
+                    new float[]{.393f, .349f, .272f+1.3f, 0, 0},
+            new float[]{.769f, .686f+0.5f, .534f, 0, 0},
+            new float[]{.189f+2.3f, .168f, .131f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1}
+                });
+                ia.SetColorMatrix(cmPicture);         
+                Graphics g = Graphics.FromImage(bmpInverted);   
+                                                            
+
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+
+                
+
+
+                g.Dispose();                            
+                pictureBox1.Image = bmpInverted;
+
+            }
+        }
+
+
+        void filter2()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("Open an Image then apply changes");
+            }
+            else
+            {
+
+                Image img = pictureBox1.Image;                           
+                Bitmap bmpInverted = new Bitmap(img.Width, img.Height);   
+                                                                        
+
+                ImageAttributes ia = new ImageAttributes();                
+                ColorMatrix cmPicture = new ColorMatrix(new float[][]       
+                {
+                    new float[]{.393f, .349f+0.5f, .272f, 0, 0},
+            new float[]{.769f+0.3f, .686f, .534f, 0, 0},
+            new float[]{.189f, .168f, .131f+0.5f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1}
+                });
+                ia.SetColorMatrix(cmPicture);           
+                Graphics g = Graphics.FromImage(bmpInverted);   
+                                                           
+
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+
+               
+
+
+                g.Dispose();                            
+                pictureBox1.Image = bmpInverted;
+
+            }
+        }
+
+
+        void filter3()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("Open an Image then apply changes");
+            }
+            else
+            {
+
+                Image img = pictureBox1.Image;                            
+                Bitmap bmpInverted = new Bitmap(img.Width, img.Height);   
+                                                                        
+
+                ImageAttributes ia = new ImageAttributes();                
+                ColorMatrix cmPicture = new ColorMatrix(new float[][]       
+                {
+                    new float[]{.393f+0.3f, .349f, .272f, 0, 0},
+            new float[]{.769f, .686f+0.2f, .534f, 0, 0},
+            new float[]{.189f, .168f, .131f+0.9f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1}
+                });
+                ia.SetColorMatrix(cmPicture);          
+                Graphics g = Graphics.FromImage(bmpInverted);   
+                                                           
+
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+
+               
+
+                g.Dispose();                         
+                pictureBox1.Image = bmpInverted;
+
+            }
+        }
+
+        private static Image cropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openImage();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            reload();
+            filter1();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            reload();
+            filter2();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            reload();
+            filter3();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = bmp;
+            }
+            
+            else
+            {
+
+                MessageBox.Show("No Image");
+
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+                bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+                pictureBox1.Image = bmp;
+            }
+            else
+            {
+
+                MessageBox.Show("No Image");
+
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+                bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+                pictureBox1.Image = bmp;
+            }
+            else
+            {
+
+                MessageBox.Show("No Image");
+
+            }
+        }
+    }
+}
